87986219c459afc98dd02122c78bd75ae3f7d9e0
Enzo Agustín López <enzo@enzo-kubuntu-22.04.3>
2024-02-09 17:03:36 -0300

    send authentication status to frontend

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello, backend!");
+app.post("/validate", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === "admin" && password === "admin") {
+        res.send({ redirect: "/", message: "Logged Successfully!" });
+    } else {
+        res.send({ redirect: "/login", message: "Invalid Credentials!" });
+    }
 });
 
 app.listen(5000);
